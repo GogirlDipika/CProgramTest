18d08347f2631334a50ca54a22614798637311f5
GogirlDipika <109748510+GogirlDipika@users.noreply.github.com>
2023-04-28 02:55:18 +0530

    added

---
+++ src/SampleProgram1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    if (x == 10) {
+        printf("x is equal to 10\n");
+    }
+    else {
+        printf("x is not equal to 10\n");
+    }
+    return 0;
+}
