30dfcc1f4e035c07daf5f3147d0751e63eba4c65
GogirlDipika <109748510+GogirlDipika@users.noreply.github.com>
2023-04-28 03:02:09 +0530

    Update SampleProgram1.c

--- src/SampleProgram1.c
+++ src/SampleProgram1.c
@@ -3,10 +3,10 @@
 int main() {
     int x = 10;
     if (x == 10) {
-        printf("x is equal to 10\n");
+        printf("x is equal to 10.\n");
     }
     else {
-        printf("x is not equal to 10\n");
+        printf("x is not equal to 10.\n");
     }
     return 0;
 }
