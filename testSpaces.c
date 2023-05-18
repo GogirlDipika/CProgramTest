0faf7ba380cb391acf5ba6c5c4cd7264627f315b
GogirlDipika <109748510+GogirlDipika@users.noreply.github.com>
2023-05-18 21:06:21 +0530

    Create testSpaces.c

---
+++ testSpaces.c
@@ -0,0 +1,13 @@
+int main() {
+  // This line has an extra space before the opening parenthesis.
+  if ( 1 ) {
+    // This line has an extra space after the closing parenthesis.
+  }
+
+  // This line has an extra space before the opening curly brace.
+  {
+    // This line has an extra space after the closing curly brace.
+  }
+
+  return 0;
+}
