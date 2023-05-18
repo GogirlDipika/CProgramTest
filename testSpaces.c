1332d83e7b4861d4de35b1d3cbc2f4c212bcc404
GogirlDipika <109748510+GogirlDipika@users.noreply.github.com>
2023-05-18 22:13:40 +0530

    Update testSpaces.c

--- testSpaces.c
+++ testSpaces.c
@@ -12,6 +12,21 @@ int main() {
   if (i == 5)
   {
   }
+  
+  int i = 0;
+
+  // This line has 2 extra spaces before the = sign.
+  if ( i ==   0 ) {
+  }
+
+  // This line has 1 extra space after the = sign.
+  if ( i ==0 )
+  {
+  }
+
+  // This line has 3 extra spaces before and after the = sign.
+  if ( i ==    0    ) {
+  }
 
   return 0;
 }
