f1a09850fcae3f2ef48aa16b713e588f9ef27c5f
GogirlDipika <109748510+GogirlDipika@users.noreply.github.com>
2023-05-18 21:02:55 +0530

    Update Sonarproject.c

--- Sonarproject.c
+++ Sonarproject.c
@@ -33,6 +33,18 @@ int main() {
         }
         break;      // this break is confusing
     }
+    
+    // This line has an extra space before the opening parenthesis.
+  if ( 1 ) {
+    // This line has an extra space after the closing parenthesis.
+  }
+
+  // This line has an extra space before the opening curly brace.
+  {
+    // This line has an extra space after the closing curly brace.
+  }
+    
+    
     return 0;
 }
 
